690b721c7d8d3fdf452305837779d0bbb8ed5bf0
remyy1999 <112822636+remyy1999@users.noreply.github.com>
2024-03-31 17:09:09 -0400

    Update socket.py

--- confundo/socket.py
+++ confundo/socket.py
@@ -225,4 +225,25 @@ class Socket:
                 self.state = State.ERROR
                 raise RuntimeError("timeout")
         return len(data)
+def expectSynAck(self):
+    startTime = time.time()
+    while True:
+        pkt = self._recv()
+        if pkt and pkt.isAck and pkt.ackNum == self.seqNum:
+            self.base = self.seqNum
+            self.state = State.OPEN
+            break
+        if time.time() - startTime > GLOBAL_TIMEOUT:
+            self.state = State.ERROR
+            raise RuntimeError("Timeout waiting for SYN-ACK")
 
+def expectFinAck(self):
+    startTime = time.time()
+    while True:
+        pkt = self._recv()
+        if pkt and pkt.isAck and pkt.ackNum == self.seqNum:
+            self.base = self.seqNum
+            self.state = State.CLOSED
+            break
+        if time.time() - startTime > GLOBAL_TIMEOUT:
+            return  # Assume server has closed the connection
